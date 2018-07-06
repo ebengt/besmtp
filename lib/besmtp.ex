34f5e05df897ee0aa03b6eb3901f60e2ea8255fc
Bengt Kleberg <bengt.kleberg@travelping.com>
2018-07-06 17:23:37 +0200

    Add TLS

--- lib/besmtp.ex
+++ lib/besmtp.ex
@@ -5,6 +5,7 @@ defmodule Besmtp do
   Send file to receiver with subject.
   Pretend to be from environment variable $from.
   Use SMTP server from environemnt variable $smtp.
+  If there is a $password, use TLS to authenticate.
   """
 
   def arguments(argv) do
@@ -12,9 +13,19 @@ defmodule Besmtp do
     |> arguments_private
   end
 
+  def mailman_deliver(email, context), do: Mailman.deliver(email, context)
+
   def mailman_context do
-    {relay, port} = System.get_env("smtp") |> String.split(":") |> mailman_context_relay_port
-    %Mailman.Context{config: %Mailman.SmtpConfig{relay: relay, port: port, auth: :never}}
+    u = System.get_env("from")
+    p = System.get_env("password")
+
+    {r, port} = System.get_env("smtp") |> String.split(":") |> mailman_context_relay_port(p)
+    a = mailman_context_auth(p)
+    t = mailman_context_tls(p)
+
+    %Mailman.Context{
+      config: %Mailman.SmtpConfig{relay: r, username: u, password: p, port: port, auth: a, tls: t}
+    }
   end
 
   def mailman_email({to, subject, text}) do
@@ -29,33 +40,53 @@ defmodule Besmtp do
     }
   end
 
-  def main(argv), do: arguments(argv) |> main_private
+  def main(argv), do: argv |> arguments |> main_private
 
   # Private functions
 
   defp arguments_private({parsed, argv, errors}) do
     help = arguments_private_help(Keyword.fetch(parsed, :help), errors, argv)
-    arguments_private_nohelp(help, parsed, argv)
+    arguments_private(help, parsed, argv)
   end
 
-  defp arguments_private_help(:error, [], [_to]), do: :nohelp
-  defp arguments_private_help(_help, _errors, _argv), do: :help
-
-  defp arguments_private_nohelp(:nohelp, parsed, [to]) do
+  defp arguments_private(:nohelp, parsed, [to]) do
     subject = Keyword.fetch!(parsed, :subject)
     infile = Keyword.fetch!(parsed, :infile)
     {to, subject, infile}
   end
 
-  defp arguments_private_nohelp(:help, _parsed, _argv), do: :help
+  defp arguments_private(:help, _parsed, _argv), do: :help
 
-  defp mailman_context_relay_port([relay]), do: {relay, 25}
-  defp mailman_context_relay_port([relay, port]), do: {relay, String.to_integer(port)}
+  defp arguments_private_help(:error, [], [_to]), do: :nohelp
+  defp arguments_private_help(_help, _errors, _argv), do: :help
+
+  defp mailman_context_auth(nil), do: :never
+  defp mailman_context_auth(_password), do: :always
+
+  defp mailman_context_relay_port([relay], nil), do: {relay, 25}
+  defp mailman_context_relay_port([relay], _password), do: {relay, 587}
+  defp mailman_context_relay_port([relay, port], _), do: {relay, String.to_integer(port)}
+
+  defp mailman_context_tls(nil), do: :never
+  defp mailman_context_tls(_password), do: :always
 
   defp main_private(:help), do: usage()
 
   defp main_private(args),
-    do: read_infile(args) |> mailman_email |> Mailman.deliver(mailman_context())
+    do: args |> read_infile |> mailman_email |> main_private_deliver
+
+  defp main_private_deliver(email) do
+    {:ok, d} = mailman_deliver(email, mailman_context())
+IO.inspect(d)
+    # Mailman.Email.parse! => :load_failed, 'Failed to load NIF library: \'priv/eiconv_nif.so: cannot open shared object file: No such file or directory\''
+    # email = Mailman.Email.parse!(d)
+    # email.delivery for Date:
+    [header | _] = String.split(d, "\r\n\r\n")
+    main_private_deliver_ok(String.contains?(header, "\r\nDate: "))
+  end
+
+  defp main_private_deliver_ok(true), do: System.stop(0)
+  defp main_private_deliver_ok(false), do: System.stop(1)
 
   defp read_infile({to, subject, infile}), do: {to, subject, File.read!(infile)}
 
--- test/smtps_test.exs
+++ test/smtps_test.exs
@@ -4,6 +4,11 @@ defmodule BesmtpTest do
 
   doctest Besmtp
 
+  setup_all do
+    Mailman.TestServer.start()
+    {:ok, :ok}
+  end
+
   test "arguments/help" do
     :help = Besmtp.arguments(["--help"])
   end
@@ -21,6 +26,7 @@ defmodule BesmtpTest do
 
   test "mailman_context" do
     relay = "asdqwe"
+    System.delete_env("password")
     System.put_env("smtp", relay)
     mc = Besmtp.mailman_context()
     assert mc.config.relay === relay
@@ -38,11 +44,42 @@ defmodule BesmtpTest do
     assert mc.config.auth === :never
   end
 
+  test "mailman_context/password" do
+    relay = "asdqwe"
+    System.put_env("smtp", relay)
+    user = "kalle"
+    System.put_env("from", user)
+    password = "gustav"
+    System.put_env("password", password)
+    mc = Besmtp.mailman_context()
+    assert mc.config.relay === relay
+    assert mc.config.port === 587
+    assert mc.config.auth === :always
+    assert mc.config.username === user
+    assert mc.config.password === password
+  end
+
+  test "mailman_context/password/port" do
+    relay = "asdqwe"
+    port = 123
+    System.put_env("smtp", relay <> ":#{port}")
+    user = "kalle"
+    System.put_env("from", user)
+    password = "gustav"
+    System.put_env("password", password)
+    mc = Besmtp.mailman_context()
+    assert mc.config.relay === relay
+    assert mc.config.port === port
+    assert mc.config.auth === :always
+    assert mc.config.username === user
+    assert mc.config.password === password
+  end
+
   test "mailman_email" do
-    from = "somebody"
     to = "kalle"
     subject = "aboutit"
     text = "text message"
+    from = "somebody"
     System.put_env("from", from)
     me = Besmtp.mailman_email({to, subject, text})
     assert me.to === [to]
@@ -50,5 +87,19 @@ defmodule BesmtpTest do
     assert me.text === text
     assert me.from === from
     assert me.reply_to === from
+  end
+
+  test "deliver" do
+    to = "kalle"
+    subject = "aboutit"
+    text = "text message"
+    from = "somebody"
+    System.put_env("from", from)
+    me = Besmtp.mailman_email({to, subject, text})
+    mc = %Mailman.Context{config: %Mailman.TestConfig{}}
+    {:ok, md} = Besmtp.mailman_deliver(me, mc)
+    assert String.contains?(md, "From: " <> from)
+    assert String.contains?(md, "Subject: " <> subject)
+    assert String.contains?(md, "\r\n\r\n" <> text)
   end
 end
