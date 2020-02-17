c394b09c465c54750bfaf2a4a27af18f23b89ef6
Bengt Kleberg <bengt.kleberg@travelping.com>
2020-02-17 12:52:57 +0100

    update for new elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Besmtp.Mixfile do
     [
       app: :besmtp,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       escript: [main_module: Besmtp],
       start_permanent: Mix.env() == :prod,
       deps: deps()
